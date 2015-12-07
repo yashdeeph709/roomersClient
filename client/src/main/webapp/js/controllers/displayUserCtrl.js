b588f20e8fecdaaa60eff2d7a7bddd1158463380
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-07 22:01:21 +0530

    Display Users update with ux!

--- client/src/main/webapp/js/controllers/displayUserCtrl.js
+++ client/src/main/webapp/js/controllers/displayUserCtrl.js
@@ -5,4 +5,13 @@ app.controller('displayUserCtrl', ['$scope','$http', function($scope,$http){
 	$http.get("http://127.0.0.1:8080/getUsers").success(function(data){
 		$scope.users=data;
 	});
+	$scope.deleteUser=function(id){
+		console.log(id);
+		$http.get("http://127.0.0.1:8080/delete/"+id).success(function(data){
+			alert("User Deleted Successfully!");
+		});
+		$http.get("http://127.0.0.1:8080/getUsers").success(function(data){
+			$scope.users=data;
+		});
+	}
 }])
