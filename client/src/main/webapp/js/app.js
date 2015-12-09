5cd5d69a1558b982c3a970c5a252ab4fe17b6985
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-09 21:17:49 +0530

    url changed

--- client/src/main/webapp/js/app.js
+++ client/src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("RoomApp", ['ui.bootstrap', 'ui.router','RoomModule','UserModule'])
 
-app.constant("SERVER_ADDRESS", "http://localhost:8090/");
+app.constant("SERVER_ADDRESS", "http://localhost:8090/RoomManagement");
 app.controller('mainCtrl',function($scope){
 
 });
