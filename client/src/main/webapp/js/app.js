38f801130c6ab80bb97ee38331c27de37e8c92f0
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-10 20:30:19 +0530

    Toaster Working On Login

--- client/src/main/webapp/js/app.js
+++ client/src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("RoomApp", ['ui.bootstrap', 'ui.router','RoomModule','UserModule','ngAnimate', 'toaster'])
 
 app.constant("SERVER_ADDRESS", "http://localhost:8080/RoomManagement/");
-app.controller('mainCtrl',function($scope){
-
+app.controller('mainCtrl',function($scope,toaster){
+	toaster.pop('success', "title", "text");
 });
--- client/src/main/webapp/js/controllers/loginCtrl.js
+++ client/src/main/webapp/js/controllers/loginCtrl.js
@@ -1,11 +1,11 @@
 (function(){
 	var app=angular.module("UserModule");
-	app.controller("loginCtrl",function($scope,$http,SERVER_ADDRESS,$state){
+	app.controller("loginCtrl",function($scope,$http,SERVER_ADDRESS,$state,toaster){
 		console.log("loginCtrl loaded!")
+		
 		$scope.login=function(){
 			$http.post(SERVER_ADDRESS+"login",{email:$scope.email,password:$scope.password}).success(function(data){
-				console.log(data);
-				console.log(data.dataOne.rights);
+				if(data.dataOne!=null){
 				localStorage.setItem("Token",JSON.stringify(data.dataOne));
 				if(data.dataOne.rights==="0.0"){
 					$state.go("profileAdmin.displayUsers");
@@ -15,6 +15,9 @@
 					}else{
 						$state.go("profile");
 					}
+				}
+				}else{
+				       toaster.pop('warning', "Message", '<h5>Username or Password Incorrect</h5>', 3000, 'trustedHtml');
 				}
 			});
 		}
