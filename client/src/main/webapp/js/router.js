cfd6f862ecf247c26bd2865d0efbb59a788a730c
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-10 18:18:42 +0530

    Login Story done!

--- client/src/main/webapp/js/router.js
+++ client/src/main/webapp/js/router.js
@@ -1,33 +1,51 @@
 var app=angular.module("RoomApp");
 
 app.config(function($stateProvider,$urlRouterProvider,$httpProvider){
-	$stateProvider.state('displayUsers',{
+	
+	$stateProvider.state('profileAdmin.displayUsers',{
 		url:'/displayUsers',
 		templateUrl:'views/displayUsers.html'
 	});
-	$stateProvider.state('createRoom',{
+	$stateProvider.state('profileAdmin.createRoom',{
 		url:'/createRoom',
 		templateUrl:'views/createRoom.html'
 	});
-	$stateProvider.state('createUser',{
+	$stateProvider.state('profileAdmin.createUser',{
 		url:'/createUser',
 		templateUrl:'views/createUser.html'
 	});
-	$stateProvider.state('viewRoom',{
+	$stateProvider.state('profileAdmin.viewRoom',{
 		url:'/viewRoom',
 		templateUrl:'views/viewRoom.html'
 	});
+	$stateProvider.state('home',{
+		url:'/home',
+		templateUrl:'views/login.html'
+	});
+	$stateProvider.state('profile',{
+		url:'/profile',
+		templateUrl:'views/profile.html'
+	});
+	$stateProvider.state('profileAdmin',{
+		url: '/profileAdmin',
+		templateUrl:'views/profileAdmin.html'
+	});
+
 	$httpProvider.interceptors.push('authInterceptor');
-	$urlRouterProvider.otherwise('/displayUsers');
+	$urlRouterProvider.otherwise('/home');
 });
 app.factory('authInterceptor',function(){
 		return{
 		request:function(config){
-		var token=JSON.parse(localStorage.getItem("admin"));
+			var token=JSON.parse(localStorage.getItem("Token"));
+			console.log(token.id);
 			if(config.url.indexOf("RoomManagement")==-1){
 				return config;
 			}
 			if(config.url.indexOf("getAdmin")!=-1){
+				return config;
+			}
+			if(config.url.indexOf("login")!=-1){
 				return config;
 			}
 			if(config.url.indexOf("getUser")!=-1 && config.url.indexOf("getUsers")==-1){
@@ -36,11 +54,10 @@ app.factory('authInterceptor',function(){
 			if(config.url.indexOf("getSubAdmin")!=-1){
 				return config;
 			}
-
 			if(token){
-		 		token=token.message;
-		 		console.log(config.url+"/"+token);
+		 		token=token.id;
 		 		config.headers.authToken=token;
+		 		console.log(config)
 		 	}
 		 	return config;
 		},
