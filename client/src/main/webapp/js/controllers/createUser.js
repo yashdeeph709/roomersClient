f4ba053f534b63a7e101921c19dadeebb3425175
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-11 12:41:58 +0530

    BugCrush Season 2 done!

--- client/src/main/webapp/js/controllers/createUser.js
+++ client/src/main/webapp/js/controllers/createUser.js
@@ -1,25 +1,23 @@
 (function(){
 	var app=angular.module("UserModule");
 	app.controller('createUserCtrl',['$scope','$location','SERVER_ADDRESS','$http','toaster',function($scope,$location,SERVER_ADDRESS,$http,toaster){		
-		$scope.register=function(form)
+		$scope.register=function()
 		{
 			if($scope.password != $scope.confirmPassword)
 			{
 				toaster.pop('warning', "Message", '<h5>Password Mismatched!</h5>', 3000, 'trustedHtml');
 			}else{
-			var params={
-					name:$scope.name,
-					email:$scope.email,
-					password: $scope.password
-			};
-			$http.post(SERVER_ADDRESS+"/register",params).success(function(data){
-				if(data.status.trim()!="false"){
-				toaster.pop('success', "Message", '<h5>'+data.message+'User Created Successfully!</h5>', 3000, 'trustedHtml');
-				}else{
-				toaster.pop('warning', "Message", '<h5>User Already Exists!</h5>', 3000, 'trustedHtml');				
-				}
-			}).error(function(data){toaster.pop('error', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
-			});
+				var params={	name:$scope.name,  	email:$scope.email, password: $scope.password };
+				$http.post(SERVER_ADDRESS+"/register",params).success(function(data){
+					if(data.status.trim()!="false"){
+						toaster.pop('success', "Message", '<h5>'+data.message+'User Created Successfully!</h5>', 3000, 'trustedHtml');
+					}else{
+						toaster.pop('warning', "Message", '<h5>User Already Exists!</h5>', 3000, 'trustedHtml');				
+					}
+				}).
+				error(function(data){
+					toaster.pop('error', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
+				});
 			}
 		}
 		$scope.cancel=function()
--- client/src/main/webapp/js/controllers/mainCtrl.js
+++ client/src/main/webapp/js/controllers/mainCtrl.js
@@ -1,6 +1,12 @@
 (function(){
 var app=angular.module("RoomApp");
-app.controller('mainCtrl',['$scope','$state','toaster',function($scope,$state,toaster){
+app.controller('mainCtrl',['$scope','$state','toaster','$log','$rootScope',function($scope,$state,toaster,$log,$rootScope){
+	$rootScope.$on("$locationChangeStart", function(event, next, current) { 
+		if(next=="http://127.0.0.1:8081/#/home" && localStorage.getItem("Token")!=null){
+        	$log.info("location changing to:" + next); 
+        	next=current;
+    	}
+      });
     $scope.isAuthenticated=function(){
         return localStorage.getItem("Token")!=null?true:false;   
     }
