a2094ce6a195e2eee3bd0c4925b096ac86901cc0
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-11 15:40:11 +0530

    Confirmation done!

--- client/src/main/webapp/js/controllers/displayUserCtrl.js
+++ client/src/main/webapp/js/controllers/displayUserCtrl.js
@@ -3,6 +3,26 @@
 	
 	app.controller('displayUserCtrl', ['$scope','$http','SERVER_ADDRESS','toaster', function($scope,$http,SERVER_ADDRESS,toaster){
 		refresh();
+		$scope.confirm=function(id){
+			if(confirm("do you want to delete this user")==true){
+/*			toaster.pop({
+				title:'warning',
+				body:"Message Are you sure you want to delete this user<br><button class='btn btn-info' ng-click=''></button>",
+				onHideCallback: function () {
+					console.log("on hide called");
+						$scope.delete(id);
+		                toaster.pop({
+		                    title: 'Message',
+		                    body: 'User Deleted Successfully'
+		                });
+			      },
+			      bodyOutputType: 'trustedHtml'
+			  });
+*/			$scope.deleteUser(id);
+		}else{
+		}
+		}
+
 		$scope.deleteUser=function(id){
 			$http.get(SERVER_ADDRESS+"delete/"+id).success(function(data){
 				toaster.pop('warning', "Message", '<h5>User Deleted Successfully!</h5>', 3000, 'trustedHtml');
--- client/src/main/webapp/js/controllers/viewRoomCtrl.js
+++ client/src/main/webapp/js/controllers/viewRoomCtrl.js
@@ -9,6 +9,27 @@
 		$scope.editRoom=function(id){
 			$state.go("profileAdmin.updateRoom",{"id":id});
 		}
+		$scope.confirm=function(id){
+			if(confirm("do you want to delete this user")==true){
+/*			toaster.pop({
+				title:'warning',
+				body:"Message Are you sure you want to delete this user<br><button class='btn btn-info' ng-click=''></button>",
+				onHideCallback: function () {
+					console.log("on hide called");
+						$scope.delete(id);
+		                toaster.pop({
+		                    title: 'Message',
+		                    body: 'User Deleted Successfully'
+		                });
+			      },
+			      bodyOutputType: 'trustedHtml'
+			  });
+*/			$scope.deleteRoom(id);
+		}else{
+		}
+		}
+
+
 		$scope.deleteRoom=function(name){
 			$http.get(SERVER_ADDRESS+"deleteRoom/"+name).success(function(data){
 				toaster.pop('success', "Message", '<h5> Room Deleted Successfully!</h5>', 3000, 'trustedHtml');
