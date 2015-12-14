86710e8b60841b155a3af6d0e614d76943914661
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-14 11:53:03 +0530

    Confirm on toaster working fine! a bit needed!

--- client/src/main/webapp/js/controllers/displayUserCtrl.js
+++ client/src/main/webapp/js/controllers/displayUserCtrl.js
@@ -1,19 +1,19 @@
 (function(){
 	var app=angular.module("UserModule");
 	
-	app.controller('displayUserCtrl', ['$scope','$http','SERVER_ADDRESS','toaster', function($scope,$http,SERVER_ADDRESS,toaster){
+	app.controller('displayUserCtrl', ['$scope','$http','SERVER_ADDRESS','toaster','$rootScope', function($scope,$http,SERVER_ADDRESS,toaster,$rootScope){
 		refresh();
 		$scope.confirm=function(id){
-			if(confirm("do you want to delete this user")==true){
+			toaster.pop("warning","Message","views/toast.html",2000,'template');
+			$rootScope.$on('ok', function (event, data) {
+				console.log("I got the event");
 				$scope.deleteUser(id);
-			}else{
-			
-			}
+			});
 		}
 
 		$scope.deleteUser=function(id){
 			$http.delete(SERVER_ADDRESS+"users/"+id).success(function(data){
-				toaster.pop('warning', "Message", '<h5>User Deleted Successfully!</h5>', 1000, 'trustedHtml');
+				toaster.pop('warning', "Message", '<h5>User Deleted Successfully!</h5>', 2000, 'trustedHtml');
 			}).success(function(){
 				refresh();
 			}).error(function(data){toaster.pop('error', "Message", '<h5> Server Error!</h5>', 1000, 'trustedHtml');
---
+++ client/src/main/webapp/js/controllers/toastCtrl.js
@@ -0,0 +1,14 @@
+(function(){
+	angular.module("RoomApp").controller('abcctrl',function($rootScope){
+		console.log("abc loaded!@");
+		this.ok=function(){
+			$rootScope.$emit("ok");
+			console.log("ok called");
+		}
+		this.cancel=function(){
+			$rootScope.$emit("cancel");
+			console.log("cancel called");
+		}
+	});
+
+})();
