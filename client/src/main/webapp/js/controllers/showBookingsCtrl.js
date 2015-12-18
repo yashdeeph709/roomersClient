a053341bae8f1c12cdd4067c4ba0b9c89090f35f
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-18 11:45:10 +0530

    My bookings refresh bug resolved BugID-:RoomerIA

--- client/src/main/webapp/js/controllers/showBookingsCtrl.js
+++ client/src/main/webapp/js/controllers/showBookingsCtrl.js
@@ -7,19 +7,16 @@
 			$http.get(SERVER_ADDRESS+"booking/").success(function(data){
 				$scope.bookings=data;
 			}).error(function(data){
-				console.log("refresh called");
-				//$scope.rooms=data;
-				//toaster.pop('warning', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
+				//console.log("refresh called");
+				toaster.pop('warning', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
 			});;
 		}
 		$scope.reject=function(id){
 			$http.delete(SERVER_ADDRESS+"booking/"+id).success(function(data){
 				toaster.pop('warning', "Message", '<h5> Booking Cancelled successfully!</h5>', 3000, 'trustedHtml');
-				$scope.bookings=data;
+				refresh();
 			}).error(function(data){
-				console.log("refresh called");
-				//$scope.rooms=data;
-				//toaster.pop('warning', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
+				toaster.pop('warning', "Message", '<h5> Server Error!</h5>', 3000, 'trustedHtml');
 			});;
 		}
 	}]);	
